3fac6ce0d2a6b821d9b8047035ffa3ebb73ed38c
dawnik <dawnik@dawnik>
2019-06-26 12:17:10 +0200

    step 0, correction register.html, added method POST for form from register_views

--- default_users_app/views.py
+++ default_users_app/views.py
@@ -5,11 +5,11 @@ from django.contrib import messages
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_valis():
+        if form.is_valid():
             form.save()
             user_name = form.cleaned_data.get('user_name')
             messages.success(request, f'Account created')
             return redirect('index')
-        else:
-            form = UserCreationForm() #generation empty blank
-        return render(request, 'default_users_app/register.html', {'form': form})
+    else:
+        form = UserCreationForm() #generation empty blank
+    return render(request, 'default_users_app/register.html', {'form': form})
