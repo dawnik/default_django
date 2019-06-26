1598e734e07209d57b6e6dbd948dd02206d7fbb9
dawnik <dawnik@dawnik>
2019-06-26 12:55:43 +0200

    step 1, in django_default_view added argument template_name=name_app/name.html

--- default_users_app/urls.py
+++ default_users_app/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth import views as django_default_views
 
 urlpatterns = [
     path('register', views.register_view, name='register'),
-    path('login', django_default_views.LoginView.as_view(),     name='login'),
-    path('logout', django_default_views.LogoutView.as_view(), name='logout'),
+    path('login', django_default_views.LoginView.as_view(template_name='default_users_app/login.html'),     name='login'),
+    path('logout', django_default_views.LogoutView.as_view(template_name='default_users_app/logout.html'), name='logout'),
 ]
