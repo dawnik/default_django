5cbf304a5f1838b169e1d593fafc42d9300a0eae
dawnik <dawnik@dawnik>
2019-06-26 10:08:18 +0200

    step 3 create urls.py in users_app and added path in urls.py

--- default_django/urls.py
+++ default_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('default_page.urls')),
+    path('', include('default_users_app.urls')),
 ]
