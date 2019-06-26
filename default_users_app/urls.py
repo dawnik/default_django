f932406f31dd769401d285c537219c1edb8a088a
dawnik <dawnik@dawnik>
2019-06-26 12:41:30 +0200

    start work to login and logut, added default django view from django.contrib.auth, added path(.. LoginView...) and (... LogoutView...)

--- default_users_app/urls.py
+++ default_users_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as django_default_views
 
 urlpatterns = [
     path('register', views.register_view, name='register'),
+    path('login', django_default_views.LoginView, name='login'),
+    path('logout', django_default_views.LogoutView, name='logout'),
 ]
