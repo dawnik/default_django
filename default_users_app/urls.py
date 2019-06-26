efc95e6ece8792db128503b8eecb058ca10108bd
dawnik <dawnik@dawnik>
2019-06-26 13:54:36 +0200

    step 3, added path in urls.py and correct mistake, and added href in index.html then is chance to go profile profile_page by link

--- default_users_app/urls.py
+++ default_users_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as django_default_views
 
 urlpatterns = [
     path('register', views.register_view, name='register'),
+    path('profile', views.profile_user_view, name='profile'),
     path('login', django_default_views.LoginView.as_view(template_name='default_users_app/login.html'),     name='login'),
     path('logout', django_default_views.LogoutView.as_view(template_name='default_users_app/logout.html'), name='logout'),
 ]
