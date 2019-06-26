fc04331b5557575533e61f7bf2e34259a705f04d
dawnik <dawnik@dawnik>
2019-06-26 09:35:53 +0200

    step 1 work to create the easiest view, import django.contrib.auth.forms import UserCreationForm, create function form = UserCreationForm and return render (request, 'file.html', {'form':form})

--- default_users_app/views.py
+++ default_users_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register_view(request):
+    form = UserCreationForm()
+    return render(request, 'default_users_app/register.html', {'form': form})
