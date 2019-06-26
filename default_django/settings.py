5e7bcefd9d0c30ee5196d13f1af7c837cfa67ce4
dawnik <dawnik@dawnik>
2019-06-26 09:06:49 +0200

    step 5 create urls.py in app_name with path for urls project_name

--- default_django/settings.py
+++ default_django/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'default_django.apps.DefaultPageConfig',
+    'default_page.apps.DefaultPageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
