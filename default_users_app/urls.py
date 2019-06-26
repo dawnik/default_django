e63971e2dc6867a23eb91e0acfee14f428fa4dc0
dawnik <dawnik@dawnik>
2019-06-26 12:46:33 +0200

    step 0, correction mistake>> django.default_views.LoginView.as_view()

--- default_users_app/urls.py
+++ default_users_app/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth import views as django_default_views
 
 urlpatterns = [
     path('register', views.register_view, name='register'),
-    path('login', django_default_views.LoginView, name='login'),
-    path('logout', django_default_views.LogoutView, name='logout'),
+    path('login', django_default_views.LoginView.as_view(),     name='login'),
+    path('logout', django_default_views.LogoutView.as_view(), name='logout'),
 ]
