cd9e3bf9d6be26c8b941b379c0047d71841d993d
dawnik <dawnik@dawnik>
2019-10-11 14:44:37 +0200

    render context from only template, without name_app/page.htmlin views

--- default_page/urls.py
+++ default_page/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('context2', views.render_context_in_page_method_2, name='context2'),
     path('context/', views.render_context_in_page, name='context'),
 ]
--- default_page/views.py
+++ default_page/views.py
@@ -6,3 +6,6 @@ def index(request):
 def render_context_in_page(request):
     my_title = "Hello there..."
     return render(request, 'default_page/render_context_in_page.html', {"title": my_title})
+
+def render_context_in_page_method_2(request):
+    return render(request, 'render_context_in_page_method_2.html')
