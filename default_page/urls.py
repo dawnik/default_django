e54b2d666dfc13d42cd5fcbada863d1f4b48261d
dawnik <dawnik@dawnik>
2019-10-11 14:35:38 +0200

    render context in template/page

--- default_page/urls.py
+++ default_page/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('context/', views.render_context_in_page, name='context'),
 ]
--- default_page/views.py
+++ default_page/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'default_page/index.html')
+
+def render_context_in_page(request):
+    my_title = "Hello there..."
+    return render(request, 'default_page/render_context_in_page.html', {"title": my_title})
