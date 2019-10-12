61f43655b22979dd649a917e113b961e1d308701
dawnik <dawnik@dawnik>
2019-10-12 14:55:51 +0200

    added functionality user.is_authenticated

--- default_page/views.py
+++ default_page/views.py
@@ -5,7 +5,10 @@ def index(request):
 
 def render_context_in_page(request):
     my_title = "Hello there..."
-    return render(request, 'default_page/render_context_in_page.html', {"title": my_title})
+    context = {"title": "my title,"}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
+    return render(request, 'default_page/render_context_in_page.html', context)
 
 def render_context_in_page_method_2(request):
     return render(request, 'render_context_in_page_method_2.html')
