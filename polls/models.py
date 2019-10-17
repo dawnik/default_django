c9ca63e004c1365d5a64ca80d619c9154edc7245
dawnik <dawnik@dawnik>
2019-10-17 17:37:26 +0200

    added app polls

---
+++ polls/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=20)
+    published_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.published_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    choice_question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=20)
+    choice_votes = models.ImageField(default=0)
+    def __str__(self):
+        return self.choice_text
+# Dodanie metod __str__() do twoich modeli jest ważne, nie tylko dla twojej własnej wygody, gdy używasz interaktywnego
+# prompta, ale także dlatego, że reprezentacje obiektów są używane w automatycznie generowanym panelu administracyjnym Django.
