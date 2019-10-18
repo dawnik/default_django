a7e7adb507aca18862a6cd8ca13274156255767a
dawnik <dawnik@dawnik>
2019-10-18 21:50:27 +0200

    Use template system, removing rigid URLs form template, added namespaces for URLs

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('detail/<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
+from django.http import Http404
 from django.template import loader
+
 
 
 def detail(request, question_id):
     # return HttpResponse("You're looking at question %s." % question_id)
-    return render(request, "polls/question_id.html", {'question_id' : question_id})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/question_id.html", {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -23,4 +26,4 @@ def index_question(request):
     }
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(template.render(context_question, request))
-    return render(request, 'polls/polls_index.html', context_question)
+    return render(request, 'polls/index_question.html', context_question)
